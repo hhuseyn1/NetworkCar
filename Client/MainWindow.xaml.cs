4bdbd02c92207d394c4c6dab336ebea283ac3bb6
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-03-16 22:05:04 +0400

    Make a Design

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,11 +1,28 @@
-﻿using System.Windows;
+﻿using Client.Enums;
+using Client.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace Client;
 
 public partial class MainWindow : Window
 {
+    ObservableCollection<Car> Cars { get; set; }
+    ObservableCollection<string> Methods { get; set; }
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = this;
+        Cars= new ();
+        Methods = new();
+        AddMethods();
     }
+    public void AddMethods()
+    {
+        Methods.Add(HttpMethods.Get.ToString());
+        Methods.Add(HttpMethods.Delete.ToString());
+        Methods.Add(HttpMethods.Put.ToString());
+        Methods.Add(HttpMethods.Post.ToString());
+    }
+
 }
