d79697ae4aebfcec0445365307772ebdc97c5444
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-03-17 01:19:25 +0400

    Make a design

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ namespace Client;
 public partial class MainWindow : Window
 {
     public ObservableCollection<Car> Cars { get; set; }
-    public  ObservableCollection<string> Methods { get; set; }
+    public ObservableCollection<string> Methods { get; set; }
 
     //Post-Add
     //Get-null ? all : id
@@ -18,7 +18,7 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = this;
-        Cars= new ();
+        Cars = new();
         Methods = new();
         AddMethods();
     }
@@ -30,4 +30,36 @@ public partial class MainWindow : Window
         Methods.Add(HttpMethods.Post.ToString());
     }
 
+    private void MethodBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+        if (MethodBox.SelectedItem.ToString() == "Post")
+        {
+            GridAdd.Visibility = Visibility.Visible;
+            GridPut.Visibility = Visibility.Hidden;
+            GridDelete.Visibility = Visibility.Hidden;
+            GridGetT.Visibility = Visibility.Hidden;
+
+        }
+        else if (MethodBox.SelectedItem.ToString() == "Put")
+        {
+            GridPut.Visibility = Visibility.Visible;
+            GridAdd.Visibility = Visibility.Hidden;
+            GridDelete.Visibility = Visibility.Hidden;
+            GridGetT.Visibility = Visibility.Hidden;
+        }
+        else if (MethodBox.SelectedItem.ToString() == "Delete")
+        {
+            GridDelete.Visibility = Visibility.Visible;
+            GridPut.Visibility = Visibility.Hidden;
+            GridAdd.Visibility = Visibility.Hidden;
+            GridGetT.Visibility = Visibility.Hidden;
+        }
+        else if (MethodBox.SelectedItem.ToString() == "Get")
+        {
+            GridGetT.Visibility = Visibility.Visible;
+            GridPut.Visibility = Visibility.Hidden;
+            GridDelete.Visibility = Visibility.Hidden;
+            GridAdd.Visibility = Visibility.Hidden;
+        }
+    }
 }
